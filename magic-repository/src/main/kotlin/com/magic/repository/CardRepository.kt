c545eb838f1572f5d03b4d5d3d192b13b13b9c74
natanrodovalhozup <natan.rodovalho@zup.com.br>
2020-05-23 20:57:31 -0300

    add card repository

---
+++ magic-repository/src/main/kotlin/com/magic/repository/CardRepository.kt
@@ -0,0 +1,13 @@
+package com.magic.repository
+
+import com.magic.model.Card
+import com.magic.model.PlayerType
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface CardRepository: JpaRepository<Card,Long> {
+
+    fun findByPlayerType(playerType: PlayerType): List<Card>?
+
+}
