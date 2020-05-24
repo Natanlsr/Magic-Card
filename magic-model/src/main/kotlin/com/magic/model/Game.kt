cbe96d15f57e7e53186c1c6045567ef5c2c05d22
natanrodovalhozup <natan.rodovalho@zup.com.br>
2020-05-24 19:54:21 -0300

    ajusts

--- magic-model/src/main/kotlin/com/magic/model/Game.kt
+++ magic-model/src/main/kotlin/com/magic/model/Game.kt
@@ -23,9 +23,11 @@ data class Game(
     fun setCardsInPlayers(numberCards: Int = Player.MaximmumNumberCards){
         this.randomizeDecks()
         for (player in this.players){
-            when(player.playerTypeEnum!!.name){
-                PlayerTypeEnum.COMPUTER.name -> this.cardsToBuyByComputer = player.setCardsFromDeckCards(this.cardsToBuyByComputer,numberCards)
-                PlayerTypeEnum.PLAYER.name -> this.cardsToBuyByPlayers = player.setCardsFromDeckCards(this.cardsToBuyByPlayers,numberCards)
+            if(player.deck.size < Player.MaximmumNumberCards) {
+                when (player.playerTypeEnum!!.name) {
+                    PlayerTypeEnum.COMPUTER.name -> this.cardsToBuyByComputer = player.setCardsFromDeckCards(this.cardsToBuyByComputer, numberCards)
+                    PlayerTypeEnum.PLAYER.name -> this.cardsToBuyByPlayers = player.setCardsFromDeckCards(this.cardsToBuyByPlayers, numberCards)
+                }
             }
         }
     }
